91b2cd43a3d511339efdaaa7dc9dc519a24e1a4e
Mario Rojas <>
2025-02-10 17:32:20 -0500

    change

--- react-diagrams-defaults/bat.bat
+++ react-diagrams-defaults/bat.bat
@@ -1,4 +1,5 @@
 @echo off
 time /T
-npm run clean & npx tsc & npm run build & cd C:\build_diagrams\projectStormClone & git add . & git commit -m "change" & git push & cd C:\Users\mario\OneDrive\Escritorio\work\proyectos\STELA-UI-NUEVO & rmdir /s /q node_modules & yarn install & yarn upgrade @projectstorm &  npm run start 
+npm run clean & npx tsc & npm run build & cd C:\build_diagrams\projectStormClone & git add . & git commit -m "change" & git push 
+& cd C:\Users\mario\OneDrive\Escritorio\work\proyectos\STELA-UI-NUEVO & rmdir /s /q node_modules & yarn install & yarn upgrade @projectstorm &  npm run start 
 time /T 
