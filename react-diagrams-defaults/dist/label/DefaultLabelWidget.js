290eb4f1572791cc655a7b72376d2d344444b781
Mario Rojas <>
2025-02-12 11:52:13 -0500

    change

--- react-diagrams-defaults/dist/label/DefaultLabelWidget.js
+++ react-diagrams-defaults/dist/label/DefaultLabelWidget.js
@@ -10,11 +10,35 @@ var S;
 		padding: 4px 8px;
 		font-family: sans-serif;
 		user-select: none;
+		 transform : translate(${(p) => p.x}px, ${(p) => p.y}px) 
 	`;
 })(S || (S = {}));
 export class DefaultLabelWidget extends React.Component {
+    constructor() {
+        super(...arguments);
+        this.calculate = (point1, point2) => {
+            const dx = point2.position.x - point1.position.x;
+            const dy = point2.position.y - point1.position.y;
+            const dist = Math.sqrt(dx * dx + dy * dy);
+            const midX = (point1.position.x + point2.position.x) / 2;
+            const midY = (point1.position.y + point2.position.y) / 2;
+            // setDistance(dist);
+            return { x: midX, y: midY };
+        };
+        this.calpos = () => {
+            var distancer = { x: 0, y: 0 };
+            for (let j = 0; j < this.props.model.getParent().getPoints().length - 1; j++) {
+                distancer = this.calculate(this.props.model.getParent().getPoints()[j], this.props.model.getParent().getPoints()[j + 1]);
+            }
+            const labelCoordinates = {
+                x: distancer.x + 30 - 10 / 2 + this.props.model.getOptions().offsetX,
+                y: distancer.y + 10 - 10 / 2 + this.props.model.getOptions().offsetY
+            };
+            return { x: labelCoordinates.x, y: labelCoordinates.y };
+        };
+    }
     render() {
-        return React.createElement(S.Label, null, this.props.model.getOptions().label);
+        return React.createElement(S.Label, { x: this.calpos().x, y: this.calpos().y }, this.props.model.getOptions().label);
     }
 }
 //# sourceMappingURL=DefaultLabelWidget.js.map
